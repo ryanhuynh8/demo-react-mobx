0f602ad4ab5bf99fe00ff6b17717f30f24dcc984
Huy Huynh <huyhda@vng.com.vn>
2017-07-06 12:13:57 +0700

    integrate Strophe

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -1,15 +1,66 @@
 import {observable} from 'mobx'
 import axios from 'axios';
 
+var Strophe = window.Strophe;
+
 class AppStore {
     @observable messages = [] ;
     @observable message = '';
     @observable customer = {};
+    url = 'https://conversejs.org/http-bind/';
+
 
     constructor() {
         this.webSocket = new WebSocket('wss://echo.websocket.org/');
         this.webSocket.onmessage = e => this.receiveMessage(e);
+        this.connection = new Strophe.Connection(this.url);
+        this.connection.connect('huyhda@blah.im', '123', this.onConnect.bind(this));
+        this.connection.rawOutput = data => console.log('Received: ' + data);
+        this.connection.rawInput = data => console.log('Sent: ' + data);
         console.log('Initializing connection...');
+    }
+
+    onConnect(status)
+    {
+        if (status == window.Strophe.Status.CONNECTING) {
+            console.log('Strophe is connecting.');
+        } else if (status == window.Strophe.Status.CONNFAIL) {
+            console.log('Strophe failed to connect.');
+            // $('#connect').get(0).value = 'connect';
+
+        } else if (status == window.Strophe.Status.DISCONNECTING) {
+            console.log('Strophe is disconnecting.');
+        } else if (status == window.Strophe.Status.DISCONNECTED) {
+            console.log('Strophe is disconnected.');
+            // $('#connect').get(0).value = 'connect';
+        } else if (status == window.Strophe.Status.CONNECTED) {
+            console.log('Strophe is connected.');
+            var elementShow = window.Strophe.xmlElement('show', {}, 'chat');
+            var elementStatus = window.Strophe.xmlElement('status', {}, 'Hello, I\'m from ReactJS');
+            var presence = window.$pres({from: 'huyhda@blah.im', xmlns: 'jabber:client', 'xmlns:stream': 'http://etherx.jabber.org/streams', version: '1.0'})
+                .cnode(elementShow).up()
+                .cnode(elementStatus);
+            this.connection.send(presence.tree());
+            this.connection.addHandler(this.onMessage.bind(this), null, 'message', null, null,  null);
+        }
+    }
+
+    onMessage(msg) {
+        var to = msg.getAttribute('to');
+        var from = msg.getAttribute('from');
+        var type = msg.getAttribute('type');
+        var elems = msg.getElementsByTagName('body');
+
+        if (type == "chat" && elems.length > 0) {
+            var body = elems[0];
+            var text  = Strophe.getText(body);
+            console.log(text);
+            this.addMessage({text: text, from: 'they', for: 'Huy'});
+        }
+
+        // we must return true to keep the handler alive.
+        // returning false would remove it after it finishes.
+        return true;
     }
 
     addMessage(messageInfo) {
@@ -37,18 +88,17 @@ class AppStore {
     }
 
     async loadCustomer(id) {
-        var self = this;
-        var result = await axios.get(`http://foobar123.getsandbox.com/user/${id}`)
+        var result = await axios.get(`http://foobar123.getsandbox.com/user/${id}`);
         this.customer = result.data;
         console.log(result);
     }
 
-    // async loadCustomer(id) {
-    //     var self = this;
-    //     axios.get(`http://foobar123.getsandbox.com/user/${id}`).then(customer => {
-    //         self.customer = customer.data;
-    //     });
-    // }
+    loadCustomer(id) {
+        var self = this;
+        axios.get(`http://foobar123.getsandbox.com/user/${id}`).then(customer => {
+            self.customer = customer.data;
+        });
+    }
 }
 
 var appStore = new AppStore();
